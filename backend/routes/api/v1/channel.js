38cebd86055d17b16ffa8d0ed2492ae56dec59f1
DannyBrito <britodanny1997@gmail.com>
2020-07-11 17:54:20 -0400

    created action#controller for adquiring channels from specific user

--- backend/routes/api/v1/channel.js
+++ backend/routes/api/v1/channel.js
@@ -4,4 +4,6 @@ const controller = require('../../../controllers/api/v1/channels')
 
 router.route('/').post(controller.channel_create)
 
+router.route('/user_channels/:id').get(controller.get_channels,controller.get_users)
+
 module.exports = router
